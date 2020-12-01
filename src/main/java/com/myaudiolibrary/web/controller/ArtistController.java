d3292108c76981024b8db85be1c50bd0b57a5d56
Rodolphe MICHEL <rodolphem1997@gmail.com>
2020-12-01 21:18:04 +0100

    Affichage de la liste des artists fonctionne

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -3,6 +3,7 @@ package com.myaudiolibrary.web.controller;
 import com.myaudiolibrary.web.model.Artist;
 import com.myaudiolibrary.web.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,9 +15,19 @@ public class ArtistController {
     @Autowired
     private ArtistService artistService;
 
+    //findById
     @RequestMapping(value="/{id}", method= RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
     public Artist avoirArtist(@PathVariable(value="id")Long id){
         Artist art = artistService.getArtist(id);
         return art;
+    }
+
+    //affiche tout les artists
+    @RequestMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public Page<Artist> findAll(@RequestParam(value="page") Integer page,
+                                @RequestParam(value="size") Integer size,
+                                @RequestParam(defaultValue="name") String sortProperty,
+                                @RequestParam(value="sortDirection", defaultValue="ASC") String sortDirection) {
+        return artistService.getAllArtist(page, size, sortProperty, sortDirection);
     }
 }
--- src/main/java/com/myaudiolibrary/web/service/ArtistService.java
+++ src/main/java/com/myaudiolibrary/web/service/ArtistService.java
@@ -3,8 +3,12 @@ package com.myaudiolibrary.web.service;
 import com.myaudiolibrary.web.model.Artist;
 import com.myaudiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
@@ -15,6 +19,7 @@ public class ArtistService {
     @Autowired
     private ArtistRepository artistRepository;
 
+    //findByID
     public Artist getArtist(@PathVariable(value="id")Long id){
         Optional<Artist> art = artistRepository.findById(id);
         if (art.isEmpty()) {
@@ -22,5 +27,16 @@ public class ArtistService {
             throw new EntityNotFoundException("L'artist d'id: "+id+" n'a pas été trouvé");
         }
         return art.get();
+    }
+
+    //affiche tout les artists
+    public Page<Artist> getAllArtist(@RequestParam(value="page") Integer page,
+                                     @RequestParam(value="size") Integer size,
+                                     @RequestParam(defaultValue="name") String sortProperty,
+                                     @RequestParam(value="sortDirection", defaultValue="ASC") String sortDirection){
+
+        Page<Artist> allArtist = artistRepository.findAll(PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty));
+
+        return allArtist;
     }
 }
