92163138297bfc9fea9c7b1fdfc052a780d272bf
Rodolphe <rodophem1997@gmail.com>
2020-12-19 09:58:02 +0100

    suppression ancien springboot

--- src/main/java/com/myaudiolibrary/web/repository/AlbumRespository.java
+++
@@ -1,9 +0,0 @@
-package com.myaudiolibrary.web.repository;
-
-import com.myaudiolibrary.web.model.Album;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlbumRespository  extends JpaRepository<Album, Long> {
-}
