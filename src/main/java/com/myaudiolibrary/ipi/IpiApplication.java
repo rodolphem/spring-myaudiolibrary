6070724fd24234212df9057bb0a831fa3fd40e4c
Rodolphe <rodophem1997@gmail.com>
2020-12-19 10:23:51 +0100

    ajout springboot

---
+++ src/main/java/com/myaudiolibrary/ipi/IpiApplication.java
@@ -0,0 +1,13 @@
+package com.myaudiolibrary.ipi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpiApplication.class, args);
+	}
+
+}
