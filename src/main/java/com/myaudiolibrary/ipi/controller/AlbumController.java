242d04cda5eeec9a24b7f30fefdd5acfd50e9270
Rodolphe <rodophem1997@gmail.com>
2020-12-30 11:57:15 +0100

    modif album OK

--- src/main/java/com/myaudiolibrary/ipi/controller/AlbumController.java
+++ src/main/java/com/myaudiolibrary/ipi/controller/AlbumController.java
@@ -2,9 +2,11 @@ package com.myaudiolibrary.ipi.controller;
 
 import com.myaudiolibrary.ipi.exception.ArtistException;
 import com.myaudiolibrary.ipi.model.Album;
+import com.myaudiolibrary.ipi.model.Artist;
 import com.myaudiolibrary.ipi.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +28,14 @@ public class AlbumController {
     public String delAlbum(@PathVariable("id") Long id) throws ArtistException {
         this.albumService.delAlbum(id);
         return "redirect:/";
+    }
+
+    @GetMapping("/showAlbumUpdate/{id}")
+    public String showAlbumUpdate(@PathVariable (value = "id") long id, Model model){
+        Album album = albumService.getAlbumById(id);
+
+        model.addAttribute("album", album);
+        return "update_album";
     }
 
 }
--- src/main/java/com/myaudiolibrary/ipi/service/AlbumService.java
+++ src/main/java/com/myaudiolibrary/ipi/service/AlbumService.java
@@ -1,9 +1,12 @@
 package com.myaudiolibrary.ipi.service;
 
 import com.myaudiolibrary.ipi.model.Album;
+import com.myaudiolibrary.ipi.model.Artist;
 import com.myaudiolibrary.ipi.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AlbumService {
@@ -17,5 +20,16 @@ public class AlbumService {
 
     public Album saveAlbum(Album a) {
         return albumRepository.save(a);
+    }
+
+    public Album getAlbumById(long id) {
+        Optional<Album> optional = albumRepository.findById(id);
+        Album album = null;
+        if(optional.isPresent()){
+            album = optional.get();
+        }else {
+            throw new RuntimeException("Aucun album n'a été trouvé avec l'id :: " + id);
+        }
+        return album;
     }
 }
